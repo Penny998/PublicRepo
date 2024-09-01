266dae0077b8474e963c640bd715080d209fd305
Penny998 <74766255+Penny998@users.noreply.github.com>
2024-09-01 23:07:26 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 # This script prints a simple message
 
 def main():
-    print("Hello, this is a simple message from a Python script, i have changed something here!")
+    print("Hello, this is a simple message from a Python script, i have changed something here, again i have changed!")
 
 if __name__ == "__main__":
     main()
