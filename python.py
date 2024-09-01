b025c37ac4c97c0c3d0904c70aa754b29ac6ee84
Penny998 <74766255+Penny998@users.noreply.github.com>
2024-09-01 23:29:42 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
-den# This script prints a simple message
+# This script prints a simple message
 
 def main():
     print("Hello, this is a simple message from a Python script, i have changed something here, again i have changed!")
